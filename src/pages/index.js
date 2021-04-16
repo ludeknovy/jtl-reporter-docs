607a09fe7077b3cb857d8fcf1f8fb269ce08756b
Ludek Novy <ludeknovy@fastmail.com>
2021-04-16 15:38:09 +0200

    title update

--- src/pages/index.js
+++ src/pages/index.js
@@ -77,7 +77,7 @@ export default function Home() {
   const { siteConfig = {} } = context;
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`Getting more from your performance reports | ${siteConfig.title}`}
       description="Online reporting application to generate performance reports from JMeter(Taurus), Locust and other tools by either uploading JTL file or streaming data from the test run continuously.">
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">
