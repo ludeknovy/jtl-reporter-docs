9a2a1c5a2a118318b5e4977ee3fe3a8f15c62861
Ludek Novy <13610612+ludeknovy@users.noreply.github.com>
2023-01-08 12:33:12 +0100

    api keys and other docs update

--- docs/guides/administration/api-token.md
+++ docs/guides/administration/api-token.md
@@ -5,3 +5,5 @@ title: Generate new API token
 1. Go to administrations page by clicking **administrate** from user menu
 2. Click on **Api keys** tab
 3. Create new API token by clicking **Add token** and filling its description
+
+![generate api key](../../../static/img/apikey.gif)
--- sidebars.js
+++ sidebars.js
@@ -23,9 +23,11 @@ module.exports = {
       type: 'category',
       label: 'Guides',
       items: [
-        'guides/administration/api-token',
-        'guides/manual-data-upload',
-        'guides/api-data-upload',
+        {
+          type: 'category',
+          label: 'Administration',
+          items: ['guides/administration/api-token', 'guides/manual-data-upload', 'guides/api-data-upload'],
+        },
         'guides/large-file',
         'guides/aws-ecs-installation',
         'guides/aws-ec2-installation',
