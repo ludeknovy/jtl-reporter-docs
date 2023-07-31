d90522dc54748644ba5caa570b5eb58a47370b9f
Ludek Novy <13610612+ludeknovy@users.noreply.github.com>
2023-07-31 15:04:05 +0200

    used by

---
+++ src/data/USED_BY.tsx
@@ -0,0 +1,12 @@
+const USED_BY = [
+    {
+        logo: "img/used-by/redhat_logo.svg",
+        name: "Red Hat"
+    },
+    {
+        logo: "img/used-by/wunderman_thompson_logo.png",
+        name: "Wunderman Thompon"
+    },
+];
+
+export default USED_BY;
